c823bc9f82964ce13922ff55bdcd5a51d2ff8bbb
fibia <fibia@GWTN156-4>
2024-04-26 01:29:34 -0300

    Se crea main model y constantes del ejercicio 2

---
+++ tp2_MartinIgnacio/src/ar/edu/unju/fi/ejercicio2/model/Efemeride.java
@@ -0,0 +1,8 @@
+package ar.edu.unju.fi.ejercicio2.model;
+import ar.edu.unju.fi.ejercicio2.constantes.*;
+public class Efemeride {
+private int codigo;
+private Meses mes;
+private int dia;
+private String detalle;
+}
